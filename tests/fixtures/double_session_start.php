ec4967983a2b556c5c96b8482b88bfb2c983eee0
David Négrier <d.negrier@thecodingmachine.com>
2016-01-06 12:06:46 +0100

    Adding unit tests

---
+++ tests/fixtures/double_session_start.php
@@ -0,0 +1,26 @@
+<?php
+ini_set('display_errors', 'On');
+error_reporting(E_ALL);
+
+require_once(__DIR__.'/../../vendor/autoload.php');
+
+use Mouf\Utils\Session\SessionHandler\OptimisticSessionHandler;
+
+session_set_save_handler(new OptimisticSessionHandler(), true);
+
+$a = isset($_GET['a'])?$_GET['a']:null;
+$waitBeforeStart = isset($_GET['waitBeforeStart'])?$_GET['waitBeforeStart']:0;
+$waitBeforeQuit = isset($_GET['waitBeforeQuit'])?$_GET['waitBeforeQuit']:0;
+
+sleep($waitBeforeStart);
+
+session_start();
+
+if ($a) {
+    $_SESSION['a'] = $a;
+}
+
+// Second session start... that should not change the session.
+@session_start();
+
+sleep($waitBeforeQuit);
